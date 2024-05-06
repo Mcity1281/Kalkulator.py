a4eb0215f1cf3d5a9b1918123556ade0aa077907
Mcity1281 <168414328+Mcity1281@users.noreply.github.com>
2024-05-06 10:51:19 +0200

    Initial commit

---
+++ kalkulator.py/Kalkulator.py
@@ -0,0 +1,11 @@
+
+t1=8
+t2=3
+
+sum=t1+t2
+diff=t1-t2
+prod=t1*t2
+print("Verdien i varuabel t1 er ",t1," og verdien i variabel t2 er ",t2)
+print("summen til variabel t1 og t2 er",sum)
+print("Differansen til variabelene t1 og t2 er",diff)
+print("prod variabelen til t1 og t2 er",prod)   
